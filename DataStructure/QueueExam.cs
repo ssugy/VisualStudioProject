8f19bca904bbe15976f56c0d3e66b34e1021c615
YHKwon <ssugy2@gmail.com>
2022-10-27 09:40:46 +0900

    자료구조 queue 기본사용법

---
+++ DataStructure/QueueExam.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructure
+{
+    internal class QueueExam
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            Console.WriteLine(queue.Count);
+            int queueCnt = queue.Count;
+            Console.WriteLine();
+            for (int i = 0; i < queueCnt; i++)  // 이거할때 조심해야 되겠네. queue.Count이걸로 하면 디큐할때마다 갯수가 줄어들어서 결국 반밖에 못보여줌
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
