141de37b247db63a65413f159498c1fc45000f39
YHKwon <ssugy2@gmail.com>
2022-11-19 00:34:49 +0900

    C#문법 복습 시작

---
+++ CSharp_문법정리/4.5 CSharp클래스의 확장/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4._5_CSharp클래스의_확장
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
