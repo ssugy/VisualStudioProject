7c08fa61f7acf2c4c2835070d734f8a4b90c40bd
YHKwon <ssugy2@gmail.com>
2022-11-19 00:39:46 +0900

    4.5.1 중첩 클래스 사용

--- CSharp_문법정리/4.5 CSharp클래스의 확장/Program.cs
+++ CSharp_문법정리/4.5 CSharp클래스의 확장/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace _4._5_CSharp클래스의_확장
 {
+    public class HardDisk
+    {
+        // 중첩 클래스 형태
+        // 중첩 클래스를 사용하기 위해서는 이렇게 public형태로 선언해줘야 외부에서 사용 가능하다.
+        public class Platter
+        {
+            public Platter()
+            {
+
+            }
+        }
+
+        public class Head
+        {
+
+        }
+    }
+
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HardDisk.Head head = new HardDisk.Head();  
         }
     }
 }
