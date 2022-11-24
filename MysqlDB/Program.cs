96a711c2fa47f34a1783961af22a70d48c48c5ef
YHKwon <ssugy2@gmail.com>
2022-11-24 11:51:55 +0900

    Mysql기본연결 및 데이터 가져오기

--- MysqlDB/Program.cs
+++ MysqlDB/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MysqlDB
+﻿using MySql.Data.MySqlClient;
+
+namespace MysqlDB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string connectionInfo = "server=localhost;port=3306;Database=login;Uid=root;Pwd=1234";
+            using (MySqlConnection sqlcon = new MySqlConnection(connectionInfo))
+            {
+                sqlcon.Open();
+                string sql = "select * from user";
+                MySqlCommand cmd = new MySqlCommand(sql, sqlcon);
+                MySqlDataReader r = cmd.ExecuteReader();
+                while (r.Read())
+                {
+                    int id = (int)r["iduser"];
+                    string name = (string)r["name"];
+                    string address = (string)r["adress"];
+                    Console.WriteLine($"{id} // {name} // {address}");
+                }
+                r.Close();
+                sqlcon.Close();
+            }
         }
     }
 }
